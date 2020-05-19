e2ba8b142c0d6d4bd398503812fbdc670b7cb11f
William Willers <w.willers@outlook.com>
2020-05-19 10:38:41 -0400

    Criando operacao de instanciacao

--- src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
+++ src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
@@ -1,13 +1,33 @@
 package com.rhinodevs.crudbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rhinodevs.crudbackend.domain.Categoria;
+import com.rhinodevs.crudbackend.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CrudbackendApplication {
+public class CrudbackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudbackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
