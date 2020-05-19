ec0333a8aefbec76cb37d1066115115f2a37369f
William Willers <w.willers@outlook.com>
2020-05-19 13:20:27 -0400

    Ajustes no Endpoint /categorias/id

--- src/main/java/com/rhinodevs/crudbackend/services/CategoriaService.java
+++ src/main/java/com/rhinodevs/crudbackend/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rhinodevs.crudbackend.domain.Categoria;
 import com.rhinodevs.crudbackend.repositories.CategoriaRepository;
+import com.rhinodevs.crudbackend.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,10 @@ public class CategoriaService {
 	
 	public Categoria search(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 		
 	}
 
 }
+	
