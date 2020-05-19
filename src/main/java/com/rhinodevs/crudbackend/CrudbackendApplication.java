5bdc4c28e8aa7ffd959adde18e25aae31577054b
William Willers <w.willers@outlook.com>
2020-05-19 14:38:45 -0400

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
+++ src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rhinodevs.crudbackend.domain.Categoria;
 import com.rhinodevs.crudbackend.domain.Cidade;
+import com.rhinodevs.crudbackend.domain.Cliente;
+import com.rhinodevs.crudbackend.domain.Endereco;
 import com.rhinodevs.crudbackend.domain.Estado;
 import com.rhinodevs.crudbackend.domain.Produto;
+import com.rhinodevs.crudbackend.domain.enums.TipoCliente;
 import com.rhinodevs.crudbackend.repositories.CategoriaRepository;
 import com.rhinodevs.crudbackend.repositories.CidadeRepository;
+import com.rhinodevs.crudbackend.repositories.ClienteRepository;
+import com.rhinodevs.crudbackend.repositories.EnderecoRepository;
 import com.rhinodevs.crudbackend.repositories.EstadoRepository;
 import com.rhinodevs.crudbackend.repositories.ProdutoRepository;
 
@@ -20,17 +25,23 @@ import com.rhinodevs.crudbackend.repositories.ProdutoRepository;
 public class CrudbackendApplication implements CommandLineRunner {
 	
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
-	EstadoRepository estadoRepository;
+	private EstadoRepository estadoRepository;
 	
 	@Autowired
-	CidadeRepository cidadeRepository;
-
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudbackendApplication.class, args);
 	}
@@ -67,6 +78,17 @@ public class CrudbackendApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Apto 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
