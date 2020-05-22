7f1897c0a24a926147ef932f575bd1b6941e988d
William Willers <w.willers@outlook.com>
2020-05-22 16:20:28 -0400

    Inserindo Pedido

---
+++ src/main/java/com/rhinodevs/crudbackend/config/JacksonConfig.java
@@ -0,0 +1,26 @@
+package com.rhinodevs.crudbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rhinodevs.crudbackend.domain.PagamentoComBoleto;
+import com.rhinodevs.crudbackend.domain.PagamentoComCartao;
+
+@Configuration
+public class JacksonConfig {
+//Registro padrao de subclasses
+// https://stackoverflow.com/questions/41452598/overcome-can-not-construct-instance-ofinterfaceclass-without-hinting-the-pare
+	@Bean
+	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			public void configure(ObjectMapper objectMapper) {
+				objectMapper.registerSubtypes(PagamentoComCartao.class);
+				objectMapper.registerSubtypes(PagamentoComBoleto.class);
+				super.configure(objectMapper);
+			}
+		};
+		return builder;
+	}
+}
--- src/main/java/com/rhinodevs/crudbackend/services/PedidoService.java
+++ src/main/java/com/rhinodevs/crudbackend/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.rhinodevs.crudbackend.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rhinodevs.crudbackend.domain.ItemPedido;
+import com.rhinodevs.crudbackend.domain.PagamentoComBoleto;
 import com.rhinodevs.crudbackend.domain.Pedido;
+import com.rhinodevs.crudbackend.domain.enums.EstadoPagamento;
+import com.rhinodevs.crudbackend.repositories.ItemPedidoRepository;
+import com.rhinodevs.crudbackend.repositories.PagamentoRepository;
 import com.rhinodevs.crudbackend.repositories.PedidoRepository;
 import com.rhinodevs.crudbackend.services.exceptions.ObjectNotFoundException;
 
@@ -15,11 +22,44 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido search(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
 		
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
