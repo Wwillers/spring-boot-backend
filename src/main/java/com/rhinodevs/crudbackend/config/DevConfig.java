2519c7f3d2e29f52588748a7a85a1a6df43b5847
William Willers <w.willers@outlook.com>
2020-05-25 15:28:40 -0400

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/rhinodevs/crudbackend/config/DevConfig.java
+++ src/main/java/com/rhinodevs/crudbackend/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rhinodevs.crudbackend.services.DBService;
+import com.rhinodevs.crudbackend.services.EmailService;
+import com.rhinodevs.crudbackend.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
