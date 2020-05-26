d46eb9c3b1c9c7266a7388c2d616a99dcac6ed80
William Willers <w.willers@outlook.com>
2020-05-26 15:01:08 -0400

    Autorizando endpoints para perfis especificos

--- src/main/java/com/rhinodevs/crudbackend/resources/ProdutoResource.java
+++ src/main/java/com/rhinodevs/crudbackend/resources/ProdutoResource.java
@@ -25,7 +25,6 @@ public class ProdutoResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Produto> find(@PathVariable Integer id) {
-
 		//Alterado search para find - Refatorar
 		Produto obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
