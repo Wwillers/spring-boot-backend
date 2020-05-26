0dfabe95a9d444b15fa91435f1ecfd090d512044
William Willers <w.willers@outlook.com>
2020-05-26 16:33:59 -0400

    Novo endpoint para refresh Token

---
+++ src/main/java/com/rhinodevs/crudbackend/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.rhinodevs.crudbackend.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rhinodevs.crudbackend.security.JWTUtil;
+import com.rhinodevs.crudbackend.security.UserSS;
+import com.rhinodevs.crudbackend.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
