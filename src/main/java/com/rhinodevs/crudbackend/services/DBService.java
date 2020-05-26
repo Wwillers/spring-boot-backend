d14a082699f655a5a090bdc51ea25abd8f5d5c1b
William Willers <w.willers@outlook.com>
2020-05-26 13:20:51 -0400

    Salvando perfis de usuario na base de dados: ADMIN, CLIENTE

--- src/main/java/com/rhinodevs/crudbackend/services/DBService.java
+++ src/main/java/com/rhinodevs/crudbackend/services/DBService.java
@@ -20,6 +20,7 @@ import com.rhinodevs.crudbackend.domain.PagamentoComCartao;
 import com.rhinodevs.crudbackend.domain.Pedido;
 import com.rhinodevs.crudbackend.domain.Produto;
 import com.rhinodevs.crudbackend.domain.enums.EstadoPagamento;
+import com.rhinodevs.crudbackend.domain.enums.Perfil;
 import com.rhinodevs.crudbackend.domain.enums.TipoCliente;
 import com.rhinodevs.crudbackend.repositories.CategoriaRepository;
 import com.rhinodevs.crudbackend.repositories.CidadeRepository;
@@ -125,13 +126,19 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Maria Silva", "wwillers7@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "William Willers", "novusdevelopersteam@gmail.com", "28000565099", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli1.getTelefones().addAll(Arrays.asList("998552854", "35110000"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Apto 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Alguma Coisa", "2020", null, "Centro", "78555555", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
