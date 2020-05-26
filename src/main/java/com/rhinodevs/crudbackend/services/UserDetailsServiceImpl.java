f794b0029953473183becfd13fcb09beefcdbb6b
William Willers <w.willers@outlook.com>
2020-05-26 14:13:19 -0400

    Autenticacao e geracao do token JWT

---
+++ src/main/java/com/rhinodevs/crudbackend/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.rhinodevs.crudbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.rhinodevs.crudbackend.domain.Cliente;
+import com.rhinodevs.crudbackend.repositories.ClienteRepository;
+import com.rhinodevs.crudbackend.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cliente = repo.findByEmail(email);
+		if(cliente == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+	}
+
+}
