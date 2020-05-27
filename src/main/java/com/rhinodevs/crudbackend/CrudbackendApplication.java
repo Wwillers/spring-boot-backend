6699286023283edfb31c7b670cbf70ac432b05aa
William Willers <w.willers@outlook.com>
2020-05-27 16:57:28 -0400

    Enviando img via endpoint e atualizacao gitignore

--- src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
+++ src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
@@ -1,17 +1,11 @@
 package com.rhinodevs.crudbackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.rhinodevs.crudbackend.services.S3Service;
-
 @SpringBootApplication
 public class CrudbackendApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudbackendApplication.class, args);
@@ -19,7 +13,6 @@ public class CrudbackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("D:\\S3Files\\novus.png");	
 	}
 
 }
