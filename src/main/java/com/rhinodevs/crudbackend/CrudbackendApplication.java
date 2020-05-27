b93a81ee87c9a8d86cbce0b437dd600e8b4bae9d
William Willers <w.willers@outlook.com>
2020-05-27 15:07:15 -0400

    Primeiro teste e upload de arquivo AmazonS3

--- src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
+++ src/main/java/com/rhinodevs/crudbackend/CrudbackendApplication.java
@@ -1,11 +1,17 @@
 package com.rhinodevs.crudbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rhinodevs.crudbackend.services.S3Service;
+
 @SpringBootApplication
 public class CrudbackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudbackendApplication.class, args);
@@ -13,7 +19,7 @@ public class CrudbackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("D:\\S3Files\\novus.png");	
 	}
 
 }
